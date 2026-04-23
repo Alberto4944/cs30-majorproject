99dbfc84de596b456157c6a9c19544168c53db70
Alberto4944 <albertwu040@gmail.com>
2026-04-23 13:02:11 -0600

    Started on the central viewing displayer, so I am going to do p5js 3d viewer, so it looks better.

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Click and drag the mouse to view the scene from different angles.
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  background(220);
-  square(mouseX, mouseY, 50);
+  background(200);
+
+  // Enable orbiting with the mouse.
+  orbitControl();
+
+  // Draw the box.
+  drawPoint(0.638852, 0.671197, 0.129959);
+  drawPoint(0.634599, 0.536441, -0.06984);
+}
+
+function drawPoint(x,y,z) {
+  translate(x*width,y*height,z*width);
+  sphere(50);
 }
