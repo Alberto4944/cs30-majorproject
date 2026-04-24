7317d7690772559c0b053f6ad6864684f0353e72
Alberto4944 <albertwu040@gmail.com>
2026-04-24 15:25:26 -0600

    Beginning to make the central 3d viewer. Still trying to figure out how to stack a 3d on a 2d scene.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,31 @@
 // Click and drag the mouse to view the scene from different angles.
+let viewer;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight);
+  viewer = createGraphics(500, 500, WEBGL);
 }
 
 function draw() {
-  background(200);
+  viewer.background(200);
 
   // Enable orbiting with the mouse.
-  orbitControl();
+  viewer.orbitControl();
 
   // Draw the box.
   drawPoint(0.638852, 0.671197, 0.129959);
   drawPoint(0.634599, 0.536441, -0.06984);
+
+  drawTitles();
 }
 
 function drawPoint(x,y,z) {
-  translate(x*width,y*height,z*width);
-  sphere(50);
+  viewer.fill("black");
+  viewer.translate(x*width,y*height,z*width);
+  viewer.sphere(50);
+}
+
+function drawTitles() {
+  textSize(100);
+  text("Table Tennis Dashboard", width/2, 100,100);
 }
