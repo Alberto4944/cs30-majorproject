57f61e922c9357b67cf1832b6ffbbbd0a796dd7d
Alberto4944 <albertwu040@gmail.com>
2026-04-27 15:25:01 -0600

    Started working on the overlay text. I found that using HTML text with absolute display works the best, since doing 2d and 3d on p5js is strange.

--- sketch.js
+++ sketch.js
@@ -2,30 +2,23 @@
 let viewer;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  viewer = createGraphics(500, 500, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  // viewer = createGraphics(500, 500, WEBGL);
 }
 
 function draw() {
-  viewer.background(200);
+  background(200);
 
   // Enable orbiting with the mouse.
-  viewer.orbitControl();
+  orbitControl();
 
   // Draw the box.
   drawPoint(0.638852, 0.671197, 0.129959);
   drawPoint(0.634599, 0.536441, -0.06984);
-
-  drawTitles();
 }
 
 function drawPoint(x,y,z) {
-  viewer.fill("black");
-  viewer.translate(x*width,y*height,z*width);
-  viewer.sphere(50);
-}
-
-function drawTitles() {
-  textSize(100);
-  text("Table Tennis Dashboard", width/2, 100,100);
+  fill("black");
+  translate(x*width,y*height,z*width);
+  sphere(50);
 }
