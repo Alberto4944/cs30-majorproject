6f46d5932a4ab67c117ab5a8b54d1edca2ec998e
Alberto4944 <96925274+Alberto4944@users.noreply.github.com>
2026-05-01 12:16:05 -0600

    Added connections, is a little lag intensive though.

--- sketch.js
+++ sketch.js
@@ -38,9 +38,10 @@ let points = [
 ];
 
 let connections = [
-[2, 5], // 1: Nose
-  [2], // 2: Left Eye (inner)
-  [0, 7], // 3: Left Eye
+  [2, 5], // 0: Nose
+  [2], // 1: Left Eye (inner)
+  [0, 7], // 2: Left Eye
+  [2], // 3: left Eye (outer)
   [5], // 4: Right Eye (inner)
   [0, 8], // 5: Right Eye
   [5], // 6: Right Eye (outer)
@@ -49,7 +50,7 @@ let connections = [
   [10], // 9: Mouth (left)
   [9], // 10: Mouth (right)
   [12, 13, 23], // 11: Left Shoulder
-  [11, 14, 24], // 12: Right Shoulder
+  [11, 14, 24 ], // 12: Right Shoulder
   [11, 15], // 13: Left Elbow
   [12, 16], // 14: Right Elbow
   [13, 17, 19, 21], // 15: Left Wrist
@@ -58,14 +59,14 @@ let connections = [
   [16, 20], // 18: Right Pinky
   [15, 17], // 19: Left Index
   [16, 18], // 20: Right Index
-  [15], // 21: Left Thumb
+  [15], // 21: left Thumb
   [16], // 22: Right Thumb
   [11, 24, 25], // 23: Left Hip
   [12, 23, 26], // 24: Right Hip
   [23, 27], // 25: Left Knee
   [24, 28], // 26: Right Knee
-  [25, 29, 31], // 27: Left Ankle
-  [26, 30, 32], // 28: Right Ankle
+  [25, 29], // 27: Left Ankle
+  [26, 30], // 28: Right Ankle
   [27, 31], // 29: Left Heel
   [28, 32], // 30: Right Heel
   [27, 29], // 31: Left Foot Index
@@ -75,23 +76,19 @@ let connections = [
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   // viewer = createGraphics(500, 500, WEBGL);
-  console.log(points[0]);
+  console.log(connections[0].length);
+  
 }
 
 function draw() {
   background(200);
   orbitControl();
-
   for (let index = 0; index < points.length; index++) {
-    drawPoint(points[index]);
-    push();
-    strokeWeight(20);
-    for (otherIndex = 0; otherIndex < (connections[index]).length; otherIndex++) {
-      // line(points[index][0], points[index][1], points[index][2], points[otherIndex][0], points[otherIndex][1], points[otherIndex][2]);
-      console.log(index);
+    if (index) {
+      drawPoint(points[index]);
     }
+    drawConnections(index);
     
-    pop();
   }
 }
 
@@ -100,5 +97,17 @@ function drawPoint(array) {
   fill("black");
   translate(array[0]*width,array[1]*height,array[2]*width);
   sphere(5);
+  pop();
+}
+
+function drawConnections(index) {
+  push();
+  strokeWeight(2);
+  let theConnections = connections[index];
+  for (let otherPoint of theConnections) {
+    line(points[index][0]*width, points[index][1]*height, points[index][2]*width, points[otherPoint][0]*width, points[otherPoint][1]*height, points[otherPoint][2]*width);
+    // line(points[index][0]*width, points[index][1]*height, points[index][2]*width, 0, 0, 0);
+    console.log(points[otherPoint][0]*width);
+  }
   pop();
 }
